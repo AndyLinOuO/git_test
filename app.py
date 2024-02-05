8da683b2888ac5e2ffba25f891e45aec3ef4fe95
andy@tgabing.com <mlkk0105@yahoo.com>
2024-02-05 18:41:49 +0800

    second_id

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    userId = event.source.userId
+    userId = event.source.user_id
     print(userId)
     try:
         if msg == "功能":
